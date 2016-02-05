c2d45ed9b42f3e11a6e4166c74936848085ef2ac
Xeselaro <sese.tailor@gmail.com>
2016-02-05 20:57:18 +0100

    Adding WebView as v1.0

--- app/src/main/java/onl/deepspace/deepspace/MainActivity.java
+++ app/src/main/java/onl/deepspace/deepspace/MainActivity.java
@@ -2,12 +2,32 @@ package onl.deepspace.deepspace;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.WebView);
+        mWebView.loadUrl("https://deepspace.onl/");
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
